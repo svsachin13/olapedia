5f28c3bd0471e47177b58251fa0c08d8e2906279
devidatta <devidatta45@gmail.com>
2015-09-27 15:08:04 +0530

    wip

--- src/main/scala/com/actors/CoreSystem.scala
+++ src/main/scala/com/actors/CoreSystem.scala
@@ -1,6 +1,9 @@
 package com.actors
 
-import akka.actor.ActorSystem
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorSystem}
+import com.domain.Booking
+import scala.concurrent.duration._
 
 /**
  * Created by sachin on 8/25/2015.
@@ -11,4 +14,20 @@ trait CoreSystem {
   implicit lazy val system = ActorSystem("ActorSystem")
 
   sys.addShutdownHook(system.shutdown)
+}
+
+class MyActor extends Actor{
+  val system=ActorSystem.create("system1")
+  import system.dispatcher
+  override def preStart()={
+    system.scheduler.schedule(5 minutes,
+      5 minutes,
+      self,
+      "hello")
+  }
+  override def receive: Receive =
+  {
+    case cmd:Booking=>"received booking"
+    case "hello" => //check database and do the bookings using ola api before 20 minutes of the start time
+  }
 }
